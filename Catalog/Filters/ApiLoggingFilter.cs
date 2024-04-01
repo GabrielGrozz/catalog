13d80c2746f2547210140031bc0b6e3f61415ad8
GabrielGrozz <gabrielgrozz.net@gmail.com>
2024-04-01 17:53:48 -0300

    start logging

---
+++ Catalog/Filters/ApiLoggingFilter.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Catalog.Filters
+{
+    public class ApiLoggingFilter : IActionFilter
+    {
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Catalog/obj/Debug/net8.0/Catalog.AssemblyInfo.cs
+++ Catalog/obj/Debug/net8.0/Catalog.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Catalog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a85ef41672bf04b8e8da064c9347394bde270f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c723c7235977c5e8e67f7e283db2c8480dae0e52")]
 [assembly: System.Reflection.AssemblyProductAttribute("Catalog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Catalog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
